60897afb5833b75679bb29ad3d990c625095f941
ziirolag <ziirolag@users.noreply.github.com>
2026-08-15 15:18:38 +0800

    Fix duplicate refreshTask + move formatUSD/formatPrice inside PositionsView struct

--- BTCSignal/BTCSignal/ContentView.swift
+++ BTCSignal/BTCSignal/ContentView.swift
@@ -30,10 +30,6 @@ struct ContentView: View {
         Task { await refresh() }
     }
 
-    private func refreshTask() {
-        Task { await refresh() }
-    }
-
     var body: some View {
         NavigationStack {
             ZStack {
